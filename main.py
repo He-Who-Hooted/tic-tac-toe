f4ab3b80b4debec9ffbc7311628397440b7cd085
He-Who-Hooted <warwick@owlbeak.com.au>
2025-12-03 20:42:25 +1000

    Added Game class with ability to render a board in console

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Welcome to tic-tac-toe!")
+class Game:  
+    def __init__(self):
+        print("Welcome to tic-tac-toe!")
+        self.board = [[' ' for _ in range(3)] for _ in range(3)] # initialise the blank board
+    
+    def __str__(self):
+        flat_board = [cell for row in self.board for cell in row]
+        board_image = """
+   |   |   
+ {} | {} | {} 
+___|___|___
+   |   |   
+ {} | {} | {} 
+___|___|___
+   |   |   
+ {} | {} | {} 
+   |   |   
+    """.format(*flat_board)
+        return board_image
+    
+game = Game()
+print(game)
